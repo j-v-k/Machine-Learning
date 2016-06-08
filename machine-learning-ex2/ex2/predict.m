d47a67b596820627b6ba44045ac43a09c998fabb
JVK <jameshvk@gmail.com>
2016-06-08 16:03:07 -0700

    predict change

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -14,6 +14,11 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
+values = sigmoid(X *theta)
+for i = 1:length(X)+1
+    disp(value(i))
+
+
 
 for iter = 1:(m)
    
